f293d9cb04b37d8439098f162f2b7589687b98d9
David Kiss <davidakiss@gmail.com>
2015-01-20 23:41:50 -0500

    cleaning up code

--- src/main/java/com/kaviddiss/bootquartz/job/SampleJob.java
+++ src/main/java/com/kaviddiss/bootquartz/job/SampleJob.java
@@ -1,20 +1,19 @@
 package com.kaviddiss.bootquartz.job;
 
 import com.kaviddiss.bootquartz.service.SampleService;
+import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.quartz.QuartzJobBean;
 
 /**
  * Created by david on 2015-01-20.
  */
-public class SampleJob extends QuartzJobBean {
+public class SampleJob implements Job {
     @Autowired
     private SampleService service;
 
     @Override
-    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    public void execute(JobExecutionContext jobExecutionContext) {
         service.hello();
     }
 }
